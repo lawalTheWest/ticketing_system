a3d27416a45786f5a8cdcb1aa98f4c546550d94f
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2024-09-11 15:59:24 +0100

    initialize flask app and also register blueprint from routes.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,9 @@ from flask import Flask
 from .routes import routes
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,
+                template_folder='../templates',
+                static_folder='../static')
 
     '''
         register Blueprint
--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,12 @@ routes = Blueprint('routes', __name__)
 
 @routes.route('/')
 def home():
-    return "hello world!"
+    '''Defines the home function'''
+    return render_template("home.html")
 
+@routes.route('/about')
+def About():
+    '''defines the about function'''
+    return render_template('about.html')
+
+# @routes.route('/')
