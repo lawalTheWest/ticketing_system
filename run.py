448626939c9f2f53cef46d8e7c51c3c796400646
Tajudeen <lwltjdn@gmail.com>
2024-09-20 15:16:15 +0100

    updated the host and port number

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 from app import create_app
 
 app = create_app()
@@ -6,4 +7,11 @@ app = create_app()
 
 if __name__ == "__main__":
     # app.run(host='192.168.8.100', debug=True)
-    app.run()
+    # setting the host to 0.0.0.0 due to a server issue on render.com
+    host = '0.0.0.0'
+
+    # setting the env. variable default to 10000 if not set
+    port = int(os.environ.get('port', 11000))
+
+    # Run the flask app
+    app.run(host=host, port=port)
